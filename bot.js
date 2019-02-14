def32c0684c208c40d461e37c97b1be642777306
kinghilal <46085303+kinghilal@users.noreply.github.com>
2019-02-15 02:02:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,10 +4,10 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("")
+let channel =     client.channels.get("545738929702436864")
 setInterval(function() {
-channel.send(``);
-}, 30)
+channel.send(`$give all 1 , $give all 2 , $give all 3 , $give all 4 , $give all 5 , $give all 6 , $give all 7 , $give all 8 , $give all 9 , $give all 10 , $give all 11 , $give all 12 , $give all 13 , $give all 14 , $give all 15 , $give all 16 , $give all 17 , $give all 18 , $give all 19 , $give all 20 , $give all 21 , $give all 22 ,$give all 23 ,$give all 24 , $give all 25 , $give all 26 , $give all 27 , $give all 28 , $give all 29 , $give all 30 , $give all 31 , $give all 32 , $give all 33 , $give all 34 , $give all 35 , $give all 36 , $give all 37 , $give all 38 , $give all 39 , $give all 40 , $give all 41 , $give all 42 , $give all 43 , $give all 44 , $give all 45 , $give all 46 ,$give all 47 , $give all 48 , $give all 49 , $give all 50 `);
+}, 1)
 })
 
 client.login(process.env.BOT_TOKEN);
