29b3a557ceede4efc0f5bc802f301b7a700e1e99
kinghilal <46085303+kinghilal@users.noreply.github.com>
2019-02-15 02:08:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("545738929702436864")
 setInterval(function() {
-channel.send(`$give all 1 , $give all 2 , $give all 3 , $give all 4 , $give all 5 , $give all 6 , $give all 7 , $give all 8 , $give all 9 , $give all 10`);
+channel.send(`$give all 1 $give all 2 $give all 3 $give all 4 $give all 5 $give all 6 $give all 7 $give all 8 $give all 9 $give all 10`);
 }, 1)
 })
 
