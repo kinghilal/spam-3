34c2473b25e0d54edc03627585dc716e72ad2f51
kinghilal <46085303+kinghilal@users.noreply.github.com>
2019-02-18 15:42:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,11 +6,12 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("545738929702436864")
 setInterval(function() {
-channel.send(``);
+channel.send(`test test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test test`);
 channel.send(`احب͡͠͝ك̨̛͝ ͨ͐̂موت͉ͅت̢تاحب͡͠͝ك̨̛͝ ͨ͐̂موت͉ͅت̢تاحب͡͠͝ك̨̛͝ ͨ͐̂موت͉ͅت̢تاحب͡͠͝ك̨̛͝ ͨ͐̂موت͉ͅت̢تاحب͡͠͝ك̨̛͝ ͨ͐̂موت͉ͅت̢تاحب͡͠͝ك̨̛͝ ͨ͐̂موت͉ͅت̢تاحب͡͠͝ك̨̛͝ ͨ͐̂موت͉ͅت̢تاحب͡͠͝ك̨̛͝ ͨ͐̂موت͉ͅت̢تاحب͡͠͝ك̨̛͝ ͨ͐̂موت͉ͅت̢تاحب͡͠͝ك̨̛͝ ͨ͐̂موت͉ͅت̢تاحب͡͠͝ك̨̛͝ ͨ͐̂موت͉ͅت̢تاحب͡͠͝ك̨̛͝ ͨ͐̂موت͉ͅت̢ت`);
 channel.send(`test test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test test
 `);
-}, 1)
+channel.send(`test test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test testtest test test test test test test test`);
+}, 100)
 })
 
 client.login(process.env.BOT_TOKEN);
